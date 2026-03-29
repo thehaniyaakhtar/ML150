02ebc82cd684151dc1e98bc6cef40ab2fe1a8b02
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-29 18:23:31 +0530

    Create comparison_ops.py

---
+++ Element Operations/comparison_ops.py
@@ -0,0 +1,10 @@
+# Common Math Operations
+import numpy as np
+
+A = np.array([1, 2, 3])
+B = np.array([2, 2, 2])
+
+print(A > B)   # [False False True]
+print(A == B)  # [False True False]
+
+# Output is a boolean tensor 
